12f4b5e606290413d32fcd1de77907c0926d77c0
unknown <ryannyamsi23@gmail.com>
2024-07-15 18:14:15 +0100

    second commit

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = !!localStorage.getItem('token'); // Vérifiez si l'utilisateur est connecté
 
-  return isLoggedIn ? children : <Navigate to="/authform" />;
+  return isLoggedIn ? children : <Link to="/authform" />;
 };
 
 export default ProtectedRoute;
