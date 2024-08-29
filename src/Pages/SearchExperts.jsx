46a9854169529c136ea1743b28cc1cb8d57d8bd9
unknown <ryannyamsi23@gmail.com>
2024-08-29 13:54:20 +0100

    correction SearchExperts

--- src/Pages/SearchExperts.jsx
+++ src/Pages/SearchExperts.jsx
@@ -184,7 +184,7 @@ const filteredExperts = expertsData.filter((expert) => {
       <header className="mb-8 flex flex-col items-center justify-center">
         <h1 className="text-2xl sm:text-3xl font-bold mb-6">Trouvez un expert</h1>
         <div className="space-y-4 flex flex-col items-center">
-          <div className="sm:w-[650px] w-full relative flex items-center">
+          <div className="sm:w-[650px] w-full flex items-center">
             <span className="absolute inset-y-0 left-0 flex items-center pl-3 sm:pl-4 md:pl-8">
               <svg
                 className="w-5 h-5 text-gray-400"
