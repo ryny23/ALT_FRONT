b7259b5b22ad70f8e3723f3bb57ffe2157574bf7
QilinsaMaxime <maxime.epopa@qilinsa.com>
2024-09-24 09:09:18 +0100

    ctainscription

--- src/Components/CtaInscription.jsx
+++ src/Components/CtaInscription.jsx
@@ -27,10 +27,10 @@ const CtaInscription = () => {
                   <p className="pb-6 mt-6 text-l leading-8 text-gray-300">
                   Inscrivez-vous dès maintenant pour bénéficier d'un accès gratuit d'un mois à notre plateforme premium.
                   </p>
-                  <NavLink to="/authform?tab=register">
-                    <button className='bg-gradient-to-br from-slate-800 to-slate-900 text-white py-2 px-4 transition-all duration-300 rounded hover:bg-white hover:text-cyan-200'>
-                      <strong>S'inscrire</strong>
-                    </button>
+                  <NavLink to="/authform?tab=register" className='bg-gradient-to-br from-slate-800 to-slate-900 text-white py-2 px-4 transition-all duration-300 rounded hover:bg-white hover:text-cyan-200'>
+                    
+                    <strong>S'inscrire</strong>
+                    
                   </NavLink>
                 </div>
             </div>
